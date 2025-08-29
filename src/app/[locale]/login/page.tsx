726c2efdeb65d925b6138623a7ecf1d8acca3bb0
Roberto Carrasquel <rcarrasquel@makingsense.com>
2025-08-28 22:59:32 -0400

    minor changes

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -15,7 +15,6 @@ export default async function LoginPage({
   return (
     <main className="min-h-screen bg-neutral-50">
       <section className="mx-auto max-w-6xl grid md:grid-cols-2 gap-0 min-h-screen py-4 rounded-3xl">
-        {/* Imagen izquierda (oculta en mobile) */}
         <div className="relative hidden md:block h-full">
           <Image
             src="/ticket.jpg"
@@ -30,14 +29,16 @@ export default async function LoginPage({
         {/* Contenido */}
         <div className="flex flex-col justify-center p-6 md:p-10">
           <div className="mb-8 text-center md:text-left">
-            <Image
-              src="/TMT_01.png"
-              alt={t("logoAlt")}
-              width={200}
-              height={100}
-              className="mx-auto"
-            />
-            <h1 className="mt-6 text-4xl font-extrabold text-indigo-700 leading-tight">
+            <Link href={`/${locale}`}>
+              <Image
+                src="/TMT_logo.png"
+                alt={t("logoAlt")}
+                width={200}
+                height={120}
+                className="mx-auto"
+              />
+            </Link>
+            <h1 className="mt-6 text-2xl font-extrabold text-indigo-700 leading-tight">
               {t("titleLine1")} <br /> {t("titleLine2")}
             </h1>
             <p className="mt-4 text-neutral-600 max-w-prose">{t("subtitle")}</p>
@@ -45,17 +46,17 @@ export default async function LoginPage({
 
           {/* Acciones */}
           <div className="flex flex-col gap-3">
-            <button className="inline-flex items-center justify-center rounded-xl px-4 py-2.5 text-sm font-medium text-white bg-gradient-to-r from-indigo-400 to-violet-500 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-violet-400 animate__animated animate__backInUp animate__delay-1s">
+            <button className="inline-flex items-center justify-center rounded-xl px-4 py-2.5 text-sm font-medium text-white bg-gradient-to-r from-indigo-400 to-violet-500 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-violet-400 animate__animated animate__backInUp animate__delay-1s cursor-pointer">
               {t("btnClient")}
             </button>
 
-            <button className="inline-flex items-center justify-center rounded-xl px-4 py-2.5 text-sm font-medium text-white bg-gradient-to-r from-indigo-400 to-violet-500 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-violet-400 animate__animated animate__backInUp animate__delay-2s">
+            <button className="inline-flex items-center justify-center rounded-xl px-4 py-2.5 text-sm font-medium text-white bg-gradient-to-r from-indigo-400 to-violet-500 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-violet-400 animate__animated animate__backInUp animate__delay-2s cursor-pointer">
               {t("btnAdmin")}
             </button>
 
             <Link
               href={`/${locale}`}
-              className="inline-flex items-center justify-center rounded-xl px-4 py-2.5 text-sm font-medium text-violet-500 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-violet-400 animate__animated animate__backInUp animate__delay-3s"
+              className="inline-flex items-center justify-center rounded-xl px-4 py-2.5 text-sm font-medium text-violet-500 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-violet-400 animate__animated animate__backInUp animate__delay-3s cursor-pointer"
             >
               {t("cta")}
             </Link>
